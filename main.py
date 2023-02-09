fca362b91cba44c5c849f648bb489d7b94b080ce
Joshua Zhou <joshuazhou35@gmail.com>
2023-02-09 11:42:04 -0500

    Use pubsub message

--- main.py
+++ main.py
@@ -3,12 +3,8 @@ import slack
 import requests
 from datetime import datetime, timedelta
 
-OAUTH_TOKEN = os.environ["OAUTH_TOKEN"]
-EDSTEM_TOKEN = os.environ["EDSTEM_TOKEN"]
 PEOPLE_URL = os.environ["PEOPLE_URL"]
 INTERACTS_URL = os.environ["INTERACTS_URL"]
-CHAT_NAME = os.environ["CHAT_NAME"]
-PROF_STICKER = os.environ["PROF_STICKER"]
 
 SHOUTOUT_TEMPLATE = """
 %s :tada: *Edstem Shoutouts* %s :tada:
@@ -21,8 +17,8 @@ For last week:
 Onto next week! :rocket:
 """
 
-def post_message(channel, msg):
-  client = slack.WebClient(token=OAUTH_TOKEN)
+def post_message(channel, msg, oauth):
+  client = slack.WebClient(token=oauth)
   client.chat_postMessage(channel=channel, text=msg)
 
 def get_edstem_admins(people):
@@ -53,7 +49,7 @@ def get_leaders(admins, action):
   top_doers = [admins[r] for r in action if action[r] == top_count]
   return top_count, top_doers
 
-def get_message(top_repliers, top_hearted, top_viewers):
+def get_message(top_repliers, top_hearted, top_viewers, prof_sticker):
   def get_mentions(names):
     mentions = ["<@%s>" % name for name in names]
     return ", ".join(mentions)
@@ -63,8 +59,8 @@ def get_message(top_repliers, top_hearted, top_viewers):
   view_mentions = get_mentions(top_viewers[1])
 
   return SHOUTOUT_TEMPLATE % (
-    PROF_STICKER, 
-    PROF_STICKER, 
+    prof_sticker, 
+    prof_sticker, 
     reply_mentions, 
     top_repliers[0],
     heart_mentions,
@@ -74,15 +70,22 @@ def get_message(top_repliers, top_hearted, top_viewers):
   )
 
 def main(event, context):
+  data = event["attributes"]
+  OAUTH_TOKEN = data["OAUTH_TOKEN"]
+  EDSTEM_TOKEN = data["EDSTEM_TOKEN"]
+  COURSE_NUMBER = data["COURSE_NUMBER"]
+  CHAT_NAME = data["CHAT_NAME"]
+  PROF_STICKER = data["PROF_STICKER"]
+
   header = {"X-Token": EDSTEM_TOKEN}
 
-  people = requests.get(PEOPLE_URL, headers=header)
+  people = requests.get(PEOPLE_URL % COURSE_NUMBER, headers=header)
   if people.status_code != 200:
     raise RuntimeError("error fetching people: " + people.text)
   
   admins = get_edstem_admins(people.json())
 
-  interacts = requests.get(INTERACTS_URL, headers=header)
+  interacts = requests.get(INTERACTS_URL % COURSE_NUMBER, headers=header)
   if interacts.status_code != 200:
     raise RuntimeError("error fetching people: " + interacts.text)
 
@@ -95,8 +98,5 @@ def main(event, context):
   top_hearted = get_leaders(admins, hearts)
   top_viewers = get_leaders(admins, views)
 
-  message = get_message(top_repliers, top_hearted, top_viewers)
-  post_message(CHAT_NAME, message)
-
-if __name__ == "__main__":
-  main(None, None)
+  message = get_message(top_repliers, top_hearted, top_viewers, PROF_STICKER)
+  post_message(CHAT_NAME, message, OAUTH_TOKEN)
